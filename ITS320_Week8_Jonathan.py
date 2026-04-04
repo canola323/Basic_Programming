38ba2d12f0d24d8bac8cd2c3a29606313b830d82
canola323 <jonathancanola7@gmail.com>
2026-04-03 22:11:21 -0700

    Initial Commit

---
+++ ITS320_Week8_Jonathan.py
@@ -0,0 +1,10 @@
+#-------------------------------------------
+# Program Name: Student Course Registration System
+# Author: Jonathan Canola
+# Date: 04/03/2026
+#-------------------------------------------
+# Pseudocode: [See suggestions below]
+#-------------------------------------------
+# Program Inputs: username and password - admin user menu to add, remove, update and search for course - student user menu to add and drop courses
+# Program Outputs: Login to either menu or error message if login failed - display searched course - list students registerd for specific course - list couses for a specific student - student id and passwords - List all courses
+#-------------------------------------------
