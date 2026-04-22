b5c24ea7085e0b720cb4242350e9b53fefdb9f4d
canola323 <jonathancanola7@gmail.com>
2026-04-21 22:06:03 -0700

    Add CSC320_CTModule1_Jonathan.java

---
+++ CSC320_CTModule1_Jonathan.java
@@ -0,0 +1,15 @@
+public class CSC320_CTModule1_Jonathan {
+     public static void main(String[] args) {
+        String firstName = "Fred";
+        String lastName = "Flinstone";
+        String streetAddress = "0000 Make Believe Street";
+        String city = "Whoknows";
+        String zipCode = "00000";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
